168c1b243c82afc5be9a53e4dd782d30c21b4188
Yam-Fontalba <t-e2e-yam.fontalba@comprealugueagora.com.br>
2024-12-17 17:25:00 -0300

    refactor: ios elements

--- src/ios/resources/antecipar-parcelas-ios.js
+++ src/ios/resources/antecipar-parcelas-ios.js
@@ -2,10 +2,9 @@ output.anteciparParcelas = {
     financialSolutions: ' Soluções\ \nfinanceiras',
     myLoans: ' Meus empréstimos Confira detalhes sobre o crédito que contratou e suas propostas em análise. ',
     consignedCredit: 'flatlist-setActive-item-0',
-    moreDetails: 'Crédito Consignado 400012637  Mais detalhes',
+    moreDetails: 'flatlist-setActive-item-0',
     plotDetails: 'card-values',
     advanceInstallments: 'parcela-details-button-continue',
     checkbox: 'checkbox',
-    continueButton: 'Continuar',
-    generateTicket: 'Gerar boleto'
+    continueButton: 'test-button-continue',
 }
